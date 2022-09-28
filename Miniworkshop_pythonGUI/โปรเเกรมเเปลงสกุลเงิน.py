d21bcb0a55e39456b67684d33242aa7dd9499999
Teerapoom <teerapoom.kh@mail.wu.ac.th>
2022-09-29 00:49:23 +0700

    add code

---
+++ Miniworkshop_pythonGUI/โปรเเกรมเเปลงสกุลเงิน.py
@@ -0,0 +1,14 @@
+# 1 บาท = 0.027 ยูโร
+# 1 บาท  = 3.80  เยน
+# 1 บาท  = 0.026  ดอลล่า
+# 1 บาท  = 0.024  ปอนด์
+
+from tkinter import *
+from tkinter import ttk
+root = Tk()
+root.title("โปรเเกรมเเปลงสกุลเงิน")
+
+# Input
+money = IntVar()
+Label(text="จำนวนเงิน (THB)",font=30,padx=10).grid(row=0,sticky=W)
+et1 = Entry(font=30,width=30,textvariable=money).grid(row=0,column=1)
